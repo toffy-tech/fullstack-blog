c54e55c4a934b081684d1fc79deef47f535c33d8
TatSu <tatsuya0316777@outlook.jp>
2023-09-23 16:25:37 +0900

    implement CRUD-API

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -29,7 +29,7 @@ export const POST = async (req: Request, res: NextResponse) => {
   try {
     const { title, description } = await req.json();
     await main();
-    const post = await prisma.post.create({ data: { title, description } }); //全記事取得
+    const post = await prisma.post.create({ data: { title, description } }); //記事の作成
     return NextResponse.json({ message: "Success", post }, { status: 201 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
